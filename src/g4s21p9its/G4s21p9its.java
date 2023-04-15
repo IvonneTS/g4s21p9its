0e65488ac27739126825638c7756b6d0e75d40aa
ivonne <>
2023-04-14 19:58:01 -0500

    DTO

---
+++ src/g4s21p9its/G4s21p9its.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g4s21p9its;
+
+/**
+ *
+ * @author tesoem
+ */
+public class G4s21p9its {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new JFprincipal().setVisible(true);
+    }
+    
+}
